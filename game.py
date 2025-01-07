de0c2c9d0f6edc70ec3235e6fc308730d6fe42d1
djhulleman <171265253+djhulleman@users.noreply.github.com>
2025-01-07 12:19:16 +0100

    project klaar

--- game.py
+++ game.py
@@ -1,6 +1,44 @@
 from models import *
 from board import *
+import random
+
 
 # create game board and show it
-board = Board('gameboards/Rushhour6x6_1.csv', 6)
-board.print()
+board = Board('gameboards/Rushhour9x9_6.csv', 9)
+
+import random
+
+def solve(board):
+    XX = board.cars["X"] 
+    complete = False
+    n = 0
+    max_iterations = 10000000  # Prevent infinite loops
+
+    while not complete and n < max_iterations:
+        random_car = random.choice(list(board.cars.keys()))  
+        random_move = random.choice([1, 2])
+
+        # Check if the move is valid
+
+        board.move(random_car, random_move) 
+        n += 1
+        print(f"Move {n}: Car {random_car} moved {'left/up' if random_move == 1 else 'right/down'}")
+
+        # Check if the "X" car has reached the exit
+        if XX.col + XX.length - 1 == board.size:
+            complete = True
+        # The "X" car is one step before the exit and the final position is empty
+        elif XX.col == board.size - 2 and board.board[XX.row - 1][board.size - 1] == '_':
+            complete = True
+
+    if complete:
+        print(f"Puzzle solved in {n} moves!")
+        board.print()
+    else:
+        print("Failed to solve the puzzle within the maximum number of iterations.")
+
+
+
+
+
+solve(board)
--- models.py
+++ models.py
@@ -7,24 +7,24 @@ class Board:
         # create a color range for the cars
         colors = [f"\033[38;5;{i+4}m" for i in range(1, 256)]
         # create a self to store the cars in
-        self.cars = []
+        self.size = size
+        self.cars = {}
         # open and load game bord file
         with open(filename, mode ='r')as file:
             csvFile = csv.reader(file)
             # skip first line with information about the board
             next(csvFile)
             for i, lines in enumerate(csvFile):
-                # store the car by using there names and add a color
-                var_name = lines[0]
-                self.var_name = Car(lines[0], lines[1], int(lines[2]), int(lines[3]), int(lines[4]), colors[i + 4])
-                self.cars.append(self.var_name)
+                # store the car by using their names and add a color
+                var_name = lines[0]  
+                car = Car(lines[0], lines[1], int(lines[2]), int(lines[3]), int(lines[4]), colors[i + 4])
+                self.cars[var_name] = car
 
-        
         # create matrix for board with all _
         self.board = [['_'] * size for _ in range(size)]
         # add the cars to the board
-        for i in range(0, len(self.cars)):
-            car = self.cars[i]
+        for CAR in self.cars:
+            car = self.cars[CAR]
             # see what the orientation is of the car and print it accordingly
             if car.orientation == "H":
                 for j in range(0, car.length):
@@ -36,9 +36,49 @@ class Board:
     def print(self):
         for row in self.board:
             print(' '.join(row))
+    
+    def check_move(self, car, direction):
+        car = self.cars[car]  # Get the car object
+        if car.orientation == "H":
+            if direction == 1:  # Moving left
+                if car.col - 2 >= 0 and self.board[car.row - 1][car.col - 2] == '_':
+                    return True
+            elif direction == 2:  # Moving right
+                if car.col + car.length - 1 < self.size and self.board[car.row - 1][car.col + car.length - 1] == '_':
+                    return True
+        elif car.orientation == "V":
+            if direction == 1:  # Moving up
+                if car.row - 2 >= 0 and self.board[car.row - 2][car.col - 1] == '_':
+                    return True
+            elif direction == 2:  # Moving down
+                if car.row + car.length - 1 < self.size and self.board[car.row + car.length - 1][car.col - 1] == '_':
+                    return True
+        return False
 
 
-
+    def move(self, car, direction):
+        if self.check_move(car, direction):
+            car = self.cars[car]
+            if car.orientation == "H":
+                if direction == 1:
+                    self.board[car.row - 1][car.col - 2] = f"{car.color}{car.car}\033[0m"
+                    self.board[car.row - 1][car.col + car.length - 2] = '_'
+                    car.col -= 1
+                if direction == 2:
+                    self.board[car.row - 1][car.col - 1] = '_'
+                    self.board[car.row - 1][car.col + car.length - 1] = f"{car.color}{car.car}\033[0m"
+                    car.col += 1
+            elif car.orientation == "V":
+                if direction == 1:
+                    self.board[car.row - 2][car.col - 1] = f"{car.color}{car.car}\033[0m"
+                    self.board[car.row + car.length - 2][car.col - 1] = '_'
+                    car.row -= 1
+                if direction == 2:
+                    self.board[car.row - 1][car.col - 1] = '_'
+                    self.board[car.row + car.length - 1][car.col - 1] = f"{car.color}{car.car}\033[0m"
+                    car.row += 1
+            else:
+                print("FOUT") 
 class Car:
     '''create a car'''
     def __init__(self, car, orientation, col, row, length, color):
