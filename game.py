e742d1a51a53aaadcbd12bcd0061f10574f7e48b
anne02s <anne.l.snijder@gmail.com>
2025-01-07 10:55:35 +0100

    comments added

--- game.py
+++ game.py
@@ -1,14 +1,6 @@
 from models import *
 from board import *
 
-#import board
-#cars = get_cars('gameboards/Rushhour6x6_1.csv')
-#board = create_board(6)
-#print(board)
-
-#board = place_cars(board, cars)
-#print(board)
-
-
+# create game board and show it
 board = Board('gameboards/Rushhour6x6_1.csv', 6)
 board.print()
--- models.py
+++ models.py
@@ -2,33 +2,37 @@ import numpy as np
 import csv
 
 class Board:
-
-
+    '''create a board with the cars in it'''
     def __init__(self, filename, size):
+        # create a color range for the cars
         colors = [f"\033[38;5;{i+4}m" for i in range(1, 256)]
-
+        # create a self to store the cars in
         self.cars = []
+        # open and load game bord file
         with open(filename, mode ='r')as file:
             csvFile = csv.reader(file)
+            # skip first line with information about the board
             next(csvFile)
             for i, lines in enumerate(csvFile):
+                # store the car by using there names and add a color
                 var_name = lines[0]
                 self.var_name = Car(lines[0], lines[1], int(lines[2]), int(lines[3]), int(lines[4]), colors[i + 4])
                 self.cars.append(self.var_name)
 
+        
+        # create matrix for board with all _
         self.board = [['_'] * size for _ in range(size)]
+        # add the cars to the board
         for i in range(0, len(self.cars)):
             car = self.cars[i]
+            # see what the orientation is of the car and print it accordingly
             if car.orientation == "H":
                 for j in range(0, car.length):
                     self.board[car.row-1][car.col+j-1] = f"{car.color}{car.car}\033[0m"
-        
-        for i in range(0, len(self.cars)):
-            car = self.cars[i]
-            if car.orientation == "V":
+            elif car.orientation == "V":
                 for j in range(0, car.length):
                     self.board[car.row+j-1][car.col-1] = f"{car.color}{car.car}\033[0m"
-
+    # print board
     def print(self):
         for row in self.board:
             print(' '.join(row))
@@ -36,6 +40,7 @@ class Board:
 
 
 class Car:
+    '''create a car'''
     def __init__(self, car, orientation, col, row, length, color):
         self.car = car
         self.orientation = orientation
