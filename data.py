3e4f9eaf7bab3eb4806f9c33fdad0487716fc3df
Sebastiaan van Daalen <bassievdaalen@gmail.com>
2025-01-09 12:08:06 +0100

    csv export

---
+++ data.py
@@ -0,0 +1,27 @@
+import csv
+
+class Data:
+
+    def __init__(self):
+        self.output_data = ["car,move"]
+
+    def save_move(self, car, direction):    
+        car_name = car.car
+
+        if direction == 1:
+            self.output_data.append(f"{car_name},-1")
+        elif direction == 2:
+            self.output_data.append(f"{car_name},1")
+        else:
+            self.output_data.append("Fout")
+    
+    def export_moves(self):
+        file_name = "output.csv"
+        
+        with open(file_name, mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            for line in self.output_data:
+                writer.writerow([line])  # Each line is written as a single column
+
+        print(f"Data is geexporteerd in {file_name}")
+
