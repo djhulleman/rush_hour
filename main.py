443a97adc6425bd6de7339fbe03cd1a17794a86e
Sebastiaan van Daalen <bassievdaalen@gmail.com>
2025-01-16 13:43:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,25 @@ from rushhour.classes.data import Data
 from rushhour.visualisation.plot_solutions import plot_solution
 from rushhour.algorithms.random_with_memory import *
 from rushhour.algorithms.random_with_plot import solve_with_visualization
+from rushhour.algorithms.improve_solution import *
 
 
 size = 6
 game = 1
 
 board_file = f"gameboards/Rushhour{size}x{size}_{game}.csv"
-solution_file = "solutions/solutions6x6_1.csv"
+solution_file = "output.csv"
 
-# Create data process 
+# Create data processes
 data = Data()
+memory = Memory()
 # create game board
 board = Board(f'{board_file}', size, data)
 
-finished = random_with_memory(board)
-
-print(finished.data.output_data)
-finished.data.export_moves()
-#solve_with_visualization(board)
+random_with_memory(board, memory)
+board.data.export_moves('solutions/output.csv')
+solve_with_visualization(board)
 
 
-#solution_file = "output.csv"
-#plot_solution(board_file, solution_file)
+solution_file = "output.csv"
+plot_solution(board_file, solution_file)
