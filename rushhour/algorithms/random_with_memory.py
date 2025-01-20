c591d62d3b5ca9c7913039beb761df20b35aa0fe
anne02s <anne.l.snijder@gmail.com>
2025-01-20 11:56:00 +0100

    edit comment

--- rushhour/algorithms/random_with_memory.py
+++ rushhour/algorithms/random_with_memory.py
@@ -36,7 +36,7 @@ def random_with_memory(board, memory):
 
         s += 1
         complete = board.check_finish()
-        if s%50000 == 0:
+        # if s%50000 == 0:
             # print(f"loading, {s} steps")
 
     if complete:
