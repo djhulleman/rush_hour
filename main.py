5f8bfad49babc9e9ebf9c2cd5732054d3bdc0b48
anne02s <anne.l.snijder@gmail.com>
2025-01-29 14:10:18 +0100

    main

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 
 from rushhour.visualisation.UserInterface import *
-from rushhour.classes.board import Board
-from rushhour.visualisation.solution_gif import plot_solution_to_gif
+
 # run userinterface te select the board and the algorithm
-#UserInterface()
-
-s = 6
-g = 1
-board = Board(f"gameboards/Rushhour{s}x{s}_{g}.csv", s)
-
-plot_solution_to_gif(board, "solutions/A*/solutions6x6_1.csv")
+UserInterface()
